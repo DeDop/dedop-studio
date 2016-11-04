c0e5d5645fabea7e30460c142744578b62f30cbe
Hans Permana <hans.permana@gmail.com>
2016-11-04 17:13:14 +0100

    added spectron test

--- e2e/hello_world.e2e.js
+++ e2e/hello_world.e2e.js
@@ -1,4 +1,4 @@
-import { expect } from 'chai';
+import {expect} from 'chai';
 import testUtils from './utils';
 
 describe('application launch', function () {
@@ -6,9 +6,26 @@ describe('application launch', function () {
     beforeEach(testUtils.beforeEach);
     afterEach(testUtils.afterEach);
 
+    // TODO (hans-permana, 20161104: broken. Need to understand more on how mocha and chai are being used.
+    // it('launches the application at the right size', function () {
+    //     return this.app.client.windowHandles().then(function (response) {
+    //         expect(response.value.length).to.equal(1);
+    //     }).browserWindow.getBounds().should.eventually.deep.equal({
+    //         x: 25,
+    //         y: 35,
+    //         width: 200,
+    //         height: 100
+    //     }).waitUntilTextExists('html', 'Hello')
+    //         .getTitle().should.eventually.equal('Test')
+    // });
+    it('launches the application', function () {
+        return this.app.client.windowHandles().then(function (response) {
+            expect(response.value.length).to.equal(1);
+        });
+    });
     it('shows hello world text on screen after launch', function () {
-        return this.app.client.getText('#greet').then(function (text) {
-            expect(text).to.equal('Hello World!');
+        return this.app.client.getText('#title').then(function (text) {
+            expect(text).to.equal('Welcome to DeDop Studio!');
         });
     });
 });
