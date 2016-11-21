4f7d44004634aa1385d17d61ce92098d8bb0408e
Hans Permana <hans.permana@gmail.com>
2016-11-21 16:05:19 +0100

    PICO tab placeholder

---
+++ src/renderer/footprints.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export class Footprints extends React.Component<any, any> {
+    render() {
+        return (
+            <div>
+                <h4>Footprints</h4>
+                Cesium Globe will be here
+            </div>
+        )
+    }
+}
--- src/renderer/panels.tsx
+++ src/renderer/panels.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
+
+import {Table, Column, Cell} from "@blueprintjs/table";
 import {CollapseSample} from "./collapse";
 import {ConfigurationTabs} from './tabList';
 import {PanelHeader} from './panelHeader';
 import TreeMenu from './treeMenu';
+import {Footprints} from "./footprints";
 
 export class InputDatasetPanel extends React.Component<any, any> {
     public render() {
@@ -13,7 +16,7 @@ export class InputDatasetPanel extends React.Component<any, any> {
                     <CollapseSample panelTitle="Global Metadata"/>
                 </div>
                 <div className="panel-flexbox-item" style={{backgroundColor: "#A82A2A"}}>
-                    <CollapseSample panelTitle="Footprints"/>
+                    <Footprints/>
                 </div>
             </div>
         )
@@ -37,9 +40,51 @@ export class ConfigurationPanel extends React.Component<any, any> {
 
 export class ProcessingPanel extends React.Component<any, any> {
     public render() {
+        const runCell = (rowIndex: number) => {
+            return <Cell>{`Task ${(rowIndex)}`}</Cell>
+        };
+        const configCell = (rowIndex: number) => {
+            return <Cell>{`Config ${(rowIndex)}`}</Cell>
+        };
+        const startedCell = () => {
+            return <Cell>11-11-2016</Cell>
+        };
+        const status = ["Done", "Done", "Failed", "Done", "In progress", "Queued"];
+        const statusCell = (rowIndex: number) => {
+            return <Cell>{status[rowIndex]}</Cell>
+        };
+        const processingTime = ["2:54", "1:11", "0:05", "1:30", "", ""];
+        const processingTimeCell = (rowIndex: number) => {
+            return <Cell>{processingTime[rowIndex]}</Cell>
+        };
+        const actionButtons = [<span className="pt-icon-standard pt-icon-folder-open"/>,
+            <span className="pt-icon-standard pt-icon-folder-open"/>,
+            <span className="pt-icon-standard pt-icon-warning-sign"/>,
+            <span className="pt-icon-standard pt-icon-folder-open"/>,
+            "",
+            ""];
+        const actionCell = (rowIndex: number) => {
+            return <Cell>{actionButtons[rowIndex]}</Cell>
+        };
         return (
-            <div>
-                ProcessingPanel
+            <div className="panel-flexbox">
+                <h1>Processor Invocation, Control, Observation</h1>
+                <div className="flexbox-item-pico-config" style={{backgroundColor: "#1F4B99"}}>
+                    Config
+                </div>
+                <div className="flexbox-item-pico-footprints" style={{backgroundColor: "#A82A2A"}}>
+                    <Footprints/>
+                </div>
+                <div className="flexbox-item-pico-runs" style={{backgroundColor: "#1D7324"}}>
+                    <Table numRows={6}>
+                        <Column name="Run" renderCell={runCell}/>
+                        <Column name="Configuration" renderCell={configCell}/>
+                        <Column name="Started" renderCell={startedCell}/>
+                        <Column name="Status" renderCell={statusCell}/>
+                        <Column name="Processing Time" renderCell={processingTimeCell}/>
+                        <Column name="Action" renderCell={actionCell}/>
+                    </Table>
+                </div>
             </div>
         )
     }
