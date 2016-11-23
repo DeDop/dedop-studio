3ab5bc1d290a461eec86c5f1f2555600b4473015
Hans Permana <hans.permana@gmail.com>
2016-11-23 11:45:06 +0100

    add React component unit tests with enzyme

---
+++ src/renderer/component.spec.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+
+import {HGLContainer, HGLHeader, HGLCenter, HGLFooter} from './components';
+
+describe("Main containers tests", () => {
+    it('renders <HGLContainer /> components', () => {
+        const wrapper = shallow(
+            <HGLContainer>
+                <HGLHeader/>
+                <HGLCenter/>
+                <HGLFooter/>
+            </HGLContainer>);
+        expect(wrapper.find('.hgl-container'), '.hgl-container class is expected tobe called 1 time').to.have.length(1);
+        expect(wrapper.find('div'), 'expected to have 1 div element').to.have.length(1);
+        expect(wrapper.contains(<div className="hgl-container"/>)).to.equal(true);
+    });
+
+    it('renders <HGLHeader /> components', () => {
+        const wrapper = shallow(<HGLHeader />);
+        expect(wrapper.find('.hgl-header'), '.hgl-header class is expected tobe called 1 time').to.have.length(1);
+        expect(wrapper.find('div'), 'expected to have 1 div element').to.have.length(1);
+        expect(wrapper.contains(<div className="hgl-header"/>)).to.equal(true);
+    });
+
+    it('renders <HGLCenter /> components', () => {
+        const wrapper = shallow(<HGLCenter />);
+        expect(wrapper.find('.hgl-center'), '.hgl-center class is expected tobe called 1 time').to.have.length(1);
+        expect(wrapper.find('div'), 'expected to have 1 div element').to.have.length(1);
+        expect(wrapper.contains(<div className="hgl-center"/>)).to.equal(true);
+    });
+
+    it('renders <HGLFooter /> components', () => {
+        const wrapper = shallow(<HGLFooter />);
+        expect(wrapper.find('.hgl-footer'), '.hgl-footer class is expected tobe called 1 time').to.have.length(1);
+        expect(wrapper.find('div'), 'expected to have 1 div element').to.have.length(1);
+        expect(wrapper.contains(<div className="hgl-footer"/>)).to.equal(true);
+    });
+});
