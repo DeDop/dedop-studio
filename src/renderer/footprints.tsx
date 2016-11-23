f65a1a03b0ad6a193aa762c5c592f1aa9d3ba8b9
Hans Permana <hans.permana@gmail.com>
2016-11-23 08:50:18 +0100

    added icon to footprints panel header

--- src/renderer/footprints.tsx
+++ src/renderer/footprints.tsx
@@ -6,6 +6,7 @@ export class Footprints extends React.Component<any, any> {
         return (
             <div className="dedop-collapse">
                 <div className="dedop-collapse-header">
+                    <span className="dedop-collapse-header-icon pt-icon-standard pt-icon-globe"/>
                     <span className="dedop-collapse-header-text">Footprints</span>
                 </div>
                 <div className="dedop-panel-content">
