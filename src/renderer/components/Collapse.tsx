5359850b0f11a17ea7d8a3d34d663933b357533b
Hans Permana <hans.permana@gmail.com>
2016-12-08 12:00:19 +0100

    add L1A Datasets collapsible content with some dummy data

--- src/renderer/components/Collapse.tsx
+++ src/renderer/components/Collapse.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import {Collapse, Radio} from "@blueprintjs/core";
 import {ConfigurationSingleEntry} from "./ConfigurationSingleEntry";
+import {ListBox} from "./ListBox";
 
 export interface ICollapseState {
     isOpen?: boolean;
@@ -38,6 +39,58 @@ export class DedopCollapse extends React.Component<IPanelProps, ICollapseState> 
                         [11:53:30] write ./blueprint.css<br/>
                         [11:53:30] Finished 'sass-compile-blueprint' after 2.84 s<br/>
                     </pre>
+                </Collapse>
+            </div>
+        );
+    }
+
+    private handleClick = () => {
+        this.setState({isOpen: !this.state.isOpen});
+    }
+}
+
+export interface IL1aInputListProps {
+    panelTitle: string;
+    collapseIcon?: string;
+    l1aInputFileNames: string[];
+}
+
+export class DedopL1aInputListCollapse extends React.Component<IL1aInputListProps, ICollapseState> {
+    public state = {
+        isOpen: true,
+    };
+
+    public render() {
+        const renderSingleItem = (itemIndex: number) => {
+            const fileName = this.props.l1aInputFileNames[itemIndex];
+            return (
+                <div style={{display:'flex'}}>
+                    <span>{fileName}</span>
+                </div>
+            );
+        };
+
+        return (
+            <div className="dedop-collapse">
+                <div className="dedop-collapse-header">
+                    <span className={"dedop-collapse-header-icon pt-icon-standard " + this.props.collapseIcon}/>
+                    <span className="dedop-collapse-header-text">{this.props.panelTitle}</span>
+                    <span className="dedop-collapse-header-actions">
+                    {this.state.isOpen ?
+                        <span className="pt-icon-standard pt-icon-chevron-up dedop-collapse-header-actions-icon"
+                              onClick={this.handleClick}/> :
+                        <span className="pt-icon-standard pt-icon-chevron-down dedop-collapse-header-actions-icon"
+                              onClick={this.handleClick}/>}
+                    </span>
+                </div>
+                < Collapse isOpen={this.state.isOpen}>
+                    <div className="dedop-panel-content">
+                        <label className="pt-file-upload pt-fill l1a-input-file-upload">
+                            <input type="file"/>
+                            <span className="pt-file-upload-input l1a-input-file-upload">Choose directory...</span>
+                        </label>
+                        <ListBox numItems={this.props.l1aInputFileNames.length} renderItem={renderSingleItem}/>
+                    </div>
                 </Collapse>
             </div>
         );
@@ -125,32 +178,35 @@ export class DedopRunSettingsCollapse extends React.Component<IPanelProps, IColl
                     </span>
                 </div>
                 < Collapse isOpen={this.state.isOpen}>
-                    <table>
-                        <tbody>
-                        <tr>
-                            <td>
-                                Name
-                            </td>
-                            <td style={{width: '100%'}}>
-                                <input className="pt-input pt-fill" type="text" placeholder="Process name" dir="auto"/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Configuration
-                            </td>
-                            <td style={{width: '100%'}}>
-                                <div className="pt-select pt-fill">
-                                    <select>
-                                        <option selected>Select a configuration...</option>
-                                        <option value="1">Alternate Delay-Doppler Processing</option>
-                                        <option value="2">Modified Surface Locations</option>
-                                    </select>
-                                </div>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
+                    <div className="dedop-panel-content">
+                        <table>
+                            <tbody>
+                            <tr>
+                                <td>
+                                    Name
+                                </td>
+                                <td style={{width: '100%'}}>
+                                    <input className="pt-input pt-fill" type="text" placeholder="Process name"
+                                           dir="auto"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Configuration
+                                </td>
+                                <td style={{width: '100%'}}>
+                                    <div className="pt-select pt-fill">
+                                        <select>
+                                            <option selected>Select a configuration...</option>
+                                            <option value="1">Alternate Delay-Doppler Processing</option>
+                                            <option value="2">Modified Surface Locations</option>
+                                        </select>
+                                    </div>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </Collapse>
             </div>
         );
--- src/renderer/components/Panels.tsx
+++ src/renderer/components/Panels.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 
-import {DedopCollapse, DedopRunSettingsCollapse, DedopL1aInputCollapse} from "./Collapse";
+import {DedopCollapse, DedopRunSettingsCollapse, DedopL1aInputCollapse, DedopL1aInputListCollapse} from "./Collapse";
 import {ConfigurationTabs} from './Tabs';
 import TreeMenu from './TreeMenu';
 import {FootprintsPanel} from "./FootprintsPanel";
-import {processingItems} from "../initialStates";
+import {processingItems, dummyInputL1aFiles} from "../initialStates";
 import {ProcessingTable} from "./ProcessingTable";
 import {OrdinaryPanelHeader} from "./PanelHeader";
 
@@ -13,7 +13,8 @@ export class InputDatasetPanel extends React.Component<any, any> {
         return (
             <div className="panel-flexbox">
                 <div className="panel-flexbox-item">
-                    <DedopCollapse panelTitle="L1A Datasets" collapseIcon="pt-icon-document"/>
+                    <DedopL1aInputListCollapse panelTitle="L1A Datasets" collapseIcon="pt-icon-document"
+                                               l1aInputFileNames={dummyInputL1aFiles}/>
                     <DedopCollapse panelTitle="Global Metadata" collapseIcon="pt-icon-properties"/>
                 </div>
                 <div className="panel-flexbox-item">
