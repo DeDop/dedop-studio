4b70aafc13162ab70ef18dd7a58717be34c4d17a
Hans Permana <hans.permana@gmail.com>
2016-12-09 16:05:08 +0100

    remove the add remove button from the panel header

--- src/renderer/components/PanelHeader.tsx
+++ src/renderer/components/PanelHeader.tsx
@@ -24,8 +24,6 @@ export class ConfigurationPanelHeader extends React.Component<IPanelHeaderProps,
                 <span className="dedop-collapse-header-icon pt-icon-standard pt-icon-globe"/>
                 <span className="dedop-collapse-header-text">{this.props.title}</span>
                 <div className="dedop-panel-header-configurations-buttons">
-                    <span className="pt-icon-standard pt-icon-add dedop-panel-header-configurations-button"/>
-                    <span className="pt-icon-standard pt-icon-remove dedop-panel-header-configurations-button"/>
                     <span
                         className="pt-icon-standard pt-icon-circle-arrow-up dedop-panel-header-configurations-button"/>
                     <span
