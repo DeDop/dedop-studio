a62b11b3be2ebf3d063928e94abcef43dd2dc27a
Hans Permana <hans.permana@gmail.com>
2017-01-25 18:12:23 +0100

    create rename config dialog box

--- src/renderer/components/panels/ConfigurationPanel.tsx
+++ src/renderer/components/panels/ConfigurationPanel.tsx
@@ -36,7 +36,8 @@ class ConfigurationPanel extends React.Component<IConfigurationPanelProps, any> 
     public state = {
         isNotImplementedAlertOpen: false,
         isFileNotSelectedAlertOpen: false,
-        isDialogOpen: false,
+        isAddConfigDialogOpen: false,
+        isRenameConfigDialogOpen: false,
         newConfigName: "",
         baseConfigName: "default",
         configNameValid: true
@@ -68,22 +69,38 @@ class ConfigurationPanel extends React.Component<IConfigurationPanelProps, any> 
             this.props.dispatch(selectCurrentConfig(key as string));
         };
 
-        const handleCloseDialog = () => {
+        const handleCloseAddConfigDialog = () => {
             this.setState({
-                isDialogOpen: false
+                isAddConfigDialogOpen: false
+            })
+        };
+
+        const handleCloseRenameConfigDialog = () => {
+            this.setState({
+                isRenameConfigDialogOpen: false
             })
         };
 
         const handleOpenAddConfigDialog = () => {
             this.setState({
-                isDialogOpen: true
+                isAddConfigDialogOpen: true
             })
         };
 
+        const handleOpenRenameConfigDialog = () => {
+            if (this.props.selectedConfiguration[0]) {
+                this.setState({
+                    isRenameConfigDialogOpen: true
+                })
+            } else {
+                this.setState({
+                    isFileNotSelectedAlertOpen: true
+                })
+            }
+        };
+
         const handleRenameConfig = () => {
-            this.setState({
-                isNotImplementedAlertOpen: true
-            })
+            console.log("rename config")
         };
 
         const handleDeleteConfig = () => {
@@ -99,14 +116,14 @@ class ConfigurationPanel extends React.Component<IConfigurationPanelProps, any> 
         const handleCloseAlert = () => {
             this.setState({
                 isNotImplementedAlertOpen: false,
-                isFileNotSelectedAlertOpen: false
+                isFileNotSelectedAlertOpen: false,
             })
         };
 
         const handleAddConfig = () => {
             if (this.state.newConfigName) {
                 this.props.dispatch(addConfigName(this.state.newConfigName, this.state.baseConfigName));
-                handleCloseDialog();
+                handleCloseAddConfigDialog();
                 this.setState({
                     newConfigName: ""
                 })
@@ -149,7 +166,7 @@ class ConfigurationPanel extends React.Component<IConfigurationPanelProps, any> 
                             Add
                         </button>
                         <button className="pt-button configuration-file-button"
-                                onClick={handleRenameConfig}>
+                                onClick={handleOpenRenameConfigDialog}>
                             Rename
                         </button>
                         <button className="pt-button configuration-file-button"
@@ -185,8 +202,8 @@ class ConfigurationPanel extends React.Component<IConfigurationPanelProps, any> 
                 >
                     A configuration file must be selected
                 </Alert>
-                <Dialog isOpen={this.state.isDialogOpen}
-                        onClose={handleCloseDialog}
+                <Dialog isOpen={this.state.isAddConfigDialogOpen}
+                        onClose={handleCloseAddConfigDialog}
                         title="Add a new configuration file"
                         className="dedop-dialog-body-add-config"
                 >
@@ -213,7 +230,39 @@ class ConfigurationPanel extends React.Component<IConfigurationPanelProps, any> 
                             <Button intent={Intent.PRIMARY}
                                     onClick={handleAddConfig}
                                     text="Save"/>
-                            <Button onClick={handleCloseDialog}
+                            <Button onClick={handleCloseAddConfigDialog}
+                                    text="Cancel"
+                            />
+                        </div>
+                    </div>
+                </Dialog>
+                <Dialog isOpen={this.state.isRenameConfigDialogOpen}
+                        onClose={handleCloseRenameConfigDialog}
+                        title="Rename configuration file"
+                        className="dedop-dialog-body-add-config"
+                >
+                    <div className="pt-dialog-body">
+                        <div className="dedop-dialog-parameter-item">
+                            <label className="pt-label pt-inline">
+                                <span className="dedop-dialog-parameter-label">New name</span>
+                                <input
+                                    className={"pt-input pt-inline dedop-dialog-parameter-input ".concat(this.state.configNameValid? "" : "pt-intent-danger")}
+                                    type="text"
+                                    placeholder="configuration name"
+                                    dir="auto"
+                                    value={this.state.newConfigName}
+                                    onChange={handleConfigNameEdit}
+                                    onFocus={resetConfigInvalidStatus}
+                                />
+                            </label>
+                        </div>
+                    </div>
+                    <div className="pt-dialog-footer">
+                        <div className="pt-dialog-footer-actions">
+                            <Button intent={Intent.PRIMARY}
+                                    onClick={handleRenameConfig}
+                                    text="Save"/>
+                            <Button onClick={handleCloseRenameConfigDialog}
                                     text="Cancel"
                             />
                         </div>
